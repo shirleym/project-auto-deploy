9a8d8a4467f4fb42cb34b2c998f387d69091e298
shirleym <shirem7190@gmail.com>
2020-11-27 22:10:25 -0500

    update server port in app

--- .circleci/ansible/roles/configure-server/tasks/files/app.js
+++ .circleci/ansible/roles/configure-server/tasks/files/app.js
@@ -3,5 +3,5 @@ var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
 });
-server.listen(80);
-console.log("Server running at http://127.0.0.1:80/");
+server.listen(3030);
+console.log("Server running at http://127.0.0.1:3030/");
